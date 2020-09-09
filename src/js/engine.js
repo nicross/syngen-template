1ed1e69a4e94ac69f4711cd95a398c4ad50f45c2
nicross <nicross@users.noreply.github.com>
2020-09-09 12:50:20 -0500

    Add engine.ready()

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,5 +1,15 @@
-const engine = {
-  event: {},
-  prop: {},
-  utility: {},
-}
+const engine = (() => {
+  const ready = new Promise((resolve) => {
+    document.addEventListener('DOMContentLoaded', resolve)
+  })
+
+  return {
+    prop: {},
+    ready: (callback) => {
+      return typeof callback == 'function'
+        ? ready.then(callback)
+        : ready
+    },
+    utility: {},
+  }
+})()
