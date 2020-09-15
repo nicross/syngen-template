5db526b0b477573e20892531299907c42e2a1f79
nicross <nicross@users.noreply.github.com>
2020-09-15 15:04:20 -0500

    Add engine.utility.euler.fromQuaternion()

--- src/js/engine/utility/euler.js
+++ src/js/engine/utility/euler.js
@@ -4,6 +4,64 @@ engine.utility.euler.create = function (...args) {
   return Object.create(this.prototype).construct(...args)
 }
 
+engine.utility.euler.fromQuaternion = function ({
+  w = 0,
+  x = 0,
+  y = 0,
+  z = 0,
+} = {}, sequence = engine.const.eulerToQuaternion) {
+  // SEE: http://bediyap.com/programming/convert-quaternion-to-euler-rotations/
+  const w2 = w ** 2,
+    wx = w * x,
+    wy = w * y,
+    wz = w * z,
+    x2 = x ** 2,
+    xy = x * y,
+    xz = x * z,
+    y2 = y ** 2,
+    yz = y * z,
+    z2 = z ** 2
+
+  switch (sequence) {
+    case 'XYZ':
+      return this.create({
+        pitch: Math.asin(2 * (xz + wy)),
+        roll: Math.atan2(2 * (xy - wz), w2 + x2 - y2 - z2),
+        yaw: Math.atan2(2 * (yz - wx), w2 - x2 - y2 + z2),
+      })
+    case 'XZY':
+      return this.create({
+        pitch: Math.atan2(2 * (yz + wx), w2 - x2 + y2 - z2),
+        roll: Math.atan2(2 * (xz + wy), w2 + x2 - y2 - z2),
+        yaw: Math.asin(-2 * (xy - wz)),
+      })
+    case 'YXZ':
+      return this.create({
+        pitch: Math.atan2(2 * (xy + wz), w2 - x2 + y2 - z2),
+        roll: Math.asin(-2 * (yz - wx)),
+        yaw: Math.atan2(2 * (xz + wy), w2 - x2 - y2 + z2),
+      })
+    case 'YZX':
+      return this.create({
+        pitch: Math.atan2(-2 * (yz - wx), w2 - x2 + y2 - z2),
+        roll: Math.atan2(-2 * (xz - wy), w2 + x2 - y2 - z2),
+        yaw: Math.asin(2 * (xy + wz)),
+      })
+    case 'ZXY':
+      return this.create({
+        pitch: Math.atan2(-2 * (xy - wz), w2 - x2 + y2 - z2),
+        roll: Math.asin(2 * (yz + wx)),
+        yaw: Math.atan2(-2 * (xz - wy), w2 - x2 - y2 + z2),
+      })
+    case 'ZYX':
+      return this.create({
+        pitch: Math.asin(-2 * (xz - wy)),
+        roll: Math.atan2(2 * (xy + wz), w2 + x2 - y2 - z2),
+        yaw: Math.atan2(2 * (yz + wx), 2 * (yz + wx)),
+      })
+  }
+}
+
 engine.utility.euler.prototype = {
   construct: function ({
     pitch = 0,
